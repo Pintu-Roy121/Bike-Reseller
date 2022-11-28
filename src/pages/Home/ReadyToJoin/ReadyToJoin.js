6878db5c7a0e61901d41958373069a070cf41cc0
Pintu-Roy121 <roypintu121@gmail.com>
2022-11-28 17:29:28 +0600

    reported controled

--- src/pages/Home/ReadyToJoin/ReadyToJoin.js
+++ src/pages/Home/ReadyToJoin/ReadyToJoin.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const ReadyToJoin = () => {
+    const { user } = useContext(AuthContext)
     return (
         <div>
             <div className=' flex justify-between items-center my-24 bg-blue-900 w-11/12 rounded-lg mx-auto px-24 py-6'>
@@ -10,9 +12,16 @@ const ReadyToJoin = () => {
                     <p className='text-white text-justify w-4/5 my-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                 </div>
                 <div className='w-1/2 flex justify-end'>
-                    <Link to='/signup'>
-                        <button className='btn bg-white text-black'>Register Now</button>
-                    </Link>
+                    {
+                        user?.email ?
+                            <Link to='/'>
+                                <button className='btn bg-white text-black'>Register Now</button>
+                            </Link>
+                            :
+                            <Link to='/signup'>
+                                <button className='btn bg-white text-black'>Register Now</button>
+                            </Link>
+                    }
                 </div>
             </div>
         </div>
